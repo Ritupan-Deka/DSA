3bdb28503a6049aca15000907f9da48beebb452f
Ritupan Deka <50274839+Ritupan-Deka@users.noreply.github.com>
2025-06-22 22:33:08 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (48.15%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -4,14 +4,14 @@ public:
         int left = 0, right = height.size()-1;
         int maxWater = 0;
         while(left<right){
+            int width = right-left;
+            int minheight = min(height[left],height[right]);
+            int water = width*minheight;
+            maxWater = max(maxWater,water);
             if(height[left]>height[right]){
-                int water = height[right]*(right-left); 
-                if(water>maxWater) maxWater = water;
                 right--;
             }
             else{
-                int water = height[left]*(right-left);
-                if(water>maxWater) maxWater = water;
                 left++;
             }
         }
