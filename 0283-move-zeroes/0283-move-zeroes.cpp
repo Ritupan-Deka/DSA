1acf2960ff444d94251048393eb7dab3d52ecee1
Ritupan Deka <50274839+Ritupan-Deka@users.noreply.github.com>
2025-06-28 16:10:59 +0530

    Time: 0 ms (100%), Space: 24 MB (9.78%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int ptr1 = 0;
+        int ptr2 = 0;
+        while (ptr2 < nums.size()) {
+            if (nums[ptr2] != 0) {
+                swap(nums[ptr1], nums[ptr2]);
+                ptr1++;
+            }
+            ptr2++;
+        }
+    }
+};
