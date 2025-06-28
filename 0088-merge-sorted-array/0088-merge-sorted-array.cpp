21293efba51f574f5110b60e4d5416495ec951dc
Ritupan Deka <50274839+Ritupan-Deka@users.noreply.github.com>
2025-06-28 15:17:28 +0530

    Time: 0 ms (100%), Space: 12.2 MB (70.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,12 +1,39 @@
+// class Solution {
+// public:
+//     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+//         for(int i=0;i<nums2.size();i++){
+//             nums1.pop_back();
+//         }
+//         for(int i=0;i<nums2.size();i++){
+//             nums1.push_back(nums2[i]);
+//         }
+//         sort(nums1.begin(),nums1.end());
+//     }
+// };
+
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for(int i=0;i<nums2.size();i++){
-            nums1.pop_back();
+        int ptr1=m-1;
+        int ptr2=n-1;
+        int ptr3=m+n-1;
+        while(ptr1>=0&&ptr2>=0){
+            if(nums1[ptr1]>nums2[ptr2]){
+                nums1[ptr3] = nums1[ptr1];
+                ptr1--;
+                ptr3--;
+            }
+            else{
+                nums1[ptr3] = nums2[ptr2];
+                ptr2--;
+                ptr3--;
+            }
         }
-        for(int i=0;i<nums2.size();i++){
-            nums1.push_back(nums2[i]);
+        while(ptr2>=0){
+            nums1[ptr3] = nums2[ptr2];
+            ptr3--;
+            ptr2--;
         }
-        sort(nums1.begin(),nums1.end());
     }
 };
