824f60c4ecef5e4cb26089d53a34b8a262de29b6
Ritupan Deka <50274839+Ritupan-Deka@users.noreply.github.com>
2025-07-02 23:16:14 +0530

    Time: 4 ms (18.13%), Space: 97.2 MB (86.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,25 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int min_price = INT_MAX, maxProfit = 0;
+//         for (int i = 0; i < prices.size(); i++) {
+//             if (prices[i] < min_price) 
+//                 min_price = prices[i];
+            
+//             if (prices[i] - min_price > maxProfit)
+//                 maxProfit = prices[i] - min_price;
+//         }
+//         return maxProfit;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int min_price = INT_MAX, maxProfit = 0;
         for (int i = 0; i < prices.size(); i++) {
-            if (prices[i] < min_price) 
-                min_price = prices[i];
-            
-            if (prices[i] - min_price > maxProfit)
-                maxProfit = prices[i] - min_price;
+                min_price = min(min_price,prices[i]);
+                maxProfit = max(maxProfit,prices[i] - min_price);
         }
         return maxProfit;
     }
